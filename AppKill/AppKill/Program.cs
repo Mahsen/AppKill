d3bb9787280e80267f71466bc6759c412a2dc924
Mahsen <not.select@gmail.com>
2024-03-17 12:02:37 +0330

    Create project

---
+++ AppKill/AppKill/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AppKill
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    String App = "csp_ctrlagent.exe";
+
+                    Process[] processes;
+                    processes = Process.GetProcessesByName(App);
+                    foreach (Process proc in processes)
+                    {
+                        proc.Kill();
+                        proc.WaitForExit(5000);
+                    }
+
+                    Console.WriteLine(App + " Killed");
+                    Thread.Sleep(10000);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }            
+        }
+    }
+}
