7867f44f656487b2b8f9bfcb4f06a49683af5d2e
Mahsen <not.select@gmail.com>
2024-03-18 15:47:38 +0330

    Add app name array

--- AppKill/AppKill/Program.cs
+++ AppKill/AppKill/Program.cs
@@ -1,37 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace AppKill
 {
     internal class Program
     {
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
         static void Main(string[] args)
         {
+
+            IntPtr h = Process.GetCurrentProcess().MainWindowHandle;
+            ShowWindow(h, 0);
+            String[] Apps = { "csp_ctrlagent", "csp_ctrlagentsvc" };
             while (true)
             {
-                try
+                foreach (string App in Apps)
                 {
-                    String App = "csp_ctrlagent.exe";
-
-                    Process[] processes;
-                    processes = Process.GetProcessesByName(App);
-                    foreach (Process proc in processes)
+                    try
                     {
-                        proc.Kill();
-                        proc.WaitForExit(5000);
-                    }
+                        Process[] processes;
+                        processes = Process.GetProcessesByName(App);
+                        foreach (Process proc in processes)
+                        {
+                            proc.Kill();
+                            proc.WaitForExit(5000);
+                        }
 
-                    Console.WriteLine(App + " Killed");
-                    Thread.Sleep(10000);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
+                        Console.WriteLine(App + " Killed");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    Thread.Sleep(6000);
                 }
             }            
         }
